fbc998f0fc63d318f2a55878bc8a3a93bebd3e69
Emily <lackersh@grinnell.edu>
2025-02-15 21:26:11 -0600

    Wrote the functions in SimpleStringBuffer

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -1,39 +1,79 @@
 package edu.grinnell.csc207.texteditor;
-
 /**
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+    StringBuffer stringbuffer = new StringBuffer("");
+    int cursorPosition = 0;
+    int size = 0;
+
+    /**
+     * inserts character to string buffer
+     * @param ch - character we want to add
+     */
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        stringbuffer.insert(cursorPosition, ch);
+        cursorPosition += 1;
+        size++;
     }
 
+    /**
+     * deletes a character from the string buffer
+     */
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        cursorPosition += 1;
+        size--;
     }
 
+    /**
+     * returns the cursor position
+     * @return cursorPosition
+     */
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return cursorPosition;
     }
 
+    /**
+     * moves the cursor position to the left
+     */
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        cursorPosition -= 1;
     }
 
+    /**
+     * moves the cursor position to the right
+     */
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        cursorPosition += 1;
     }
 
+    /**
+     * returns size of string buffer
+     * @return size 
+     */
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return size;
     }
 
+    /**
+     * gets character at index i and returns it
+     * @param i - index we want to check character at
+     * @return character at index i
+     * @throws IndexOutOfBoundsException when i is an invalid index greater than the size of the buffer
+     */
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        if(i <= size) {
+            return stringbuffer.charAt(i);
+        }
+        throw new IndexOutOfBoundsException("This index is out of bounds");
     }
 
+    /**
+     * returns the string buffer as a string
+     * @return string buffer 
+     */
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        return(stringbuffer.toString());
     }
 }
