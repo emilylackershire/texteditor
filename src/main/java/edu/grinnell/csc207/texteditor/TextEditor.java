d0f0ee84985bf0aa56281c59a347555e1713e7ff
Emily <lackersh@grinnell.edu>
2025-02-23 11:38:00 -0600

    Implemented most of the TextEditor

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,22 +1,80 @@
 package edu.grinnell.csc207.texteditor;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 /**
  * The driver for the TextEditor Application.
  */
 public class TextEditor {
-
+    public static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
+            int row = 0;
+            int screenWidth = 80;
+            int col = 0;
+    
+            for(int i = 0; i < buf.getSize(); i++) {
+                char ch = buf.getChar(i);
+                row = i / screenWidth;
+                col = i % screenWidth;
+                TextCharacter text = new TextCharacter(ch);
+                screen.setCharacter(row, col, text);
+            }
+            screen.refresh();
+        }
+        
     /**
      * The main entry point for the TextEditor application.
      * @param args command-line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
+    
+        Screen screen = new DefaultTerminalFactory().createScreen();
+        screen.startScreen();
+            
+        String fileName = args[0];
+        Path filePath = Paths.get(fileName);
+        GapBuffer gapbuffer = new GapBuffer();
 
-        // TODO: fill me in with a text editor TUI!
-        String path = args[0];
-        System.out.format("Loading %s...\n", path);
+        if (Files.exists(filePath)) {
+            if (Files.isRegularFile(filePath)){
+                    
+            }
+        }
+        drawBuffer(gapbuffer, screen);
+
+        boolean isRunning = true;
+        while (isRunning) {
+            KeyStroke stroke = screen.readInput();
+            KeyType key = stroke.getKeyType();
+            if (key == KeyType.Character) {
+                char ch = stroke.getCharacter();
+                gapbuffer.insert(ch);
+            }
+            else if (key == KeyType.ArrowLeft) {
+                gapbuffer.moveLeft();
+            }
+            else if (key == KeyType.ArrowRight) {
+                gapbuffer.moveRight();
+            }
+            else if (key == KeyType.Backspace) {
+                gapbuffer.delete();
+            }
+            else if (key == KeyType.Escape) {
+                isRunning = false;
+            }
+            drawBuffer(gapbuffer, screen);
+        }
+        screen.stopScreen();
     }
 }
